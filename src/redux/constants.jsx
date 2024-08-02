3dedd5e1c79d55042af48f69c11a7229546cafd7
Pawan Kumar Sahu <sahupawan9749568594@gmail.com>
2024-08-02 14:50:50 +0530

    Url updated

--- src/redux/constants.jsx
+++ src/redux/constants.jsx
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:5000";
-export const USERS_URL = "http://localhost:5000/api/users";
-export const CATEGORY_URL = "http://localhost:5000/api/category";
-export const PRODUCT_URL = "http://localhost:5000/api/products";
-export const UPLOAD_URL = "http://localhost:5000/api/upload";
-export const ORDERS_URL = "http://localhost:5000/api/orders";
-export const PAYPAL_URL = "http://localhost:5000/api/config/paypal";
+export const BASE_URL = "https://ethaum-dashboard-backend.onrender.com";
+export const USERS_URL = "https://ethaum-dashboard-backend.onrender.com/api/users";
+export const CATEGORY_URL = "https://ethaum-dashboard-backend.onrender.com/api/category";
+export const PRODUCT_URL = "https://ethaum-dashboard-backend.onrender.com/api/products";
+export const UPLOAD_URL = "https://ethaum-dashboard-backend.onrender.com/api/upload";
+export const ORDERS_URL = "https://ethaum-dashboard-backend.onrender.com/api/orders";
+export const PAYPAL_URL = "https://ethaum-dashboard-backend.onrender.com/api/config/paypal";
